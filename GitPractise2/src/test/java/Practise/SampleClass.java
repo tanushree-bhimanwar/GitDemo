7f4093fc8e753bbc46b44464b4cc0b660e9a4299
TanushreeQsp <t.v.bhimanwar13@gmail.com>
2023-08-04 21:05:52 +0530

    Demo class committed

---
+++ GitPractise2/src/test/java/Practise/SampleClass.java
@@ -0,0 +1,12 @@
+package Practise;
+
+import org.testng.annotations.Test;
+
+public class SampleClass {
+	@Test
+	public void demo()
+	{
+		System.out.println("This is GitPractise2 commit");
+	}
+
+}
